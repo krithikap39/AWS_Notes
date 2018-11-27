8d61d2b65f607e6cc9999cd3e16c9ddee3083097
krithikap39 <43339083+krithikap39@users.noreply.github.com>
2018-11-27 18:16:57 +0530

    Delete Bash Script.sh

--- Bash Script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd php git -y
-service httpd start
-chkconfig httpd on
-cd /var/www/html
-echo "<?php phpinfo();?>" > test.php
-git clone https://github.com/acloudguru/dynamodb
